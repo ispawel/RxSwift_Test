208193bb987611bd47cf7a51cdc47075f48623e9
Isakov_Pavel <isapawel@gmail.com>
2023-02-16 21:07:21 +0700

    Story 182: fix benchmarks for speed

---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by Pavel Isakov on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
